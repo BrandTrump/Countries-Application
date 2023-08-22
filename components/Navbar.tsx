96d3ea020f17eff6262bbde71469f7344bb5cfd3
Brandon Trump <brandontrump101@gmail.com>
2023-08-22 17:24:25 +1200

    style: gave a width to the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-6 shadow-md">
-      <Link href={"/"} className="text-2xl font-semibold">
-        Where in the world?
-      </Link>
+    <nav className="p-6 shadow-md">
+      <div className="flex justify-between items-center lg:w-5/6 xl:w-3/5 mx-auto">
+        <Link href={"/"} className="text-2xl font-semibold">
+          Where in the world?
+        </Link>
 
-      <div>
-        <h2 className="text-lg">Dark Mode</h2>
+        <div>
+          <h2 className="text-lg">Dark Mode</h2>
+        </div>
       </div>
     </nav>
   );
