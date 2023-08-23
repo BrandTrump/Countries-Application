b1fd0fb654e73400fd838b9efbaa479580727cd9
Brandon Trump <brandontrump101@gmail.com>
2023-08-23 23:29:11 +1200

    feat: imported dark mode button component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
+import DarkModeButton from "./DarkModeButton";
 
 function Navbar() {
   return (
-    <nav className="py-6 shadow-md">
+    <nav className="py-6 shadow-md border-b">
       <div className="flex justify-between items-center lg:w-5/6 xl:w-3/5 mx-auto">
         <Link href={"/"} className="text-2xl font-semibold">
           Where in the world?
         </Link>
 
-        <div>
-          <h2 className="text-lg">Dark Mode</h2>
-        </div>
+        <DarkModeButton />
       </div>
     </nav>
   );
