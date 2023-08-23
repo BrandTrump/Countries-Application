7c80b869a61dd8eb0a5e1404ebde9fac8c1da823
Brandon Trump <brandontrump101@gmail.com>
2023-08-23 22:31:04 +1200

    style: removed left & right padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className="p-6 shadow-md">
+    <nav className="py-6 shadow-md">
       <div className="flex justify-between items-center lg:w-5/6 xl:w-3/5 mx-auto">
         <Link href={"/"} className="text-2xl font-semibold">
           Where in the world?
