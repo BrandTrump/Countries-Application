510ea37c887c3a0c11c8d764829d3d8bf4ca61b3
Brandon Trump <brandontrump101@gmail.com>
2023-08-28 16:38:51 +1200

    feat: changed to fetch country by cca3

--- helpers/fetchCountry.ts
+++ helpers/fetchCountry.ts
@@ -1,5 +1,5 @@
 export async function fetchCountry(country: string) {
-  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  const res = await fetch(`https://restcountries.com/v3.1/alpha/${country}`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
