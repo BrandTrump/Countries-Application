1b6f0f120ac91fca96886629bde997daca87f54a
Brandon Trump <brandontrump101@gmail.com>
2023-08-30 16:48:58 +1200

    feat: included field names for faster response

--- helpers/fetchAllCountries.ts
+++ helpers/fetchAllCountries.ts
@@ -1,5 +1,7 @@
 export async function fetchAllCountries() {
-  const res = await fetch("https://restcountries.com/v3.1/all");
+  const res = await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca3"
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
--- helpers/fetchCountryByRegion.ts
+++ helpers/fetchCountryByRegion.ts
@@ -1,5 +1,7 @@
 export async function fetchCountryByRegion(region: string) {
-  const res = await fetch(`https://restcountries.com/v3.1/region/${region}`);
+  const res = await fetch(
+    `https://restcountries.com/v3.1/region/${region}?fields=name,flags,population,region,capital,cca3`
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
